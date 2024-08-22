008db6a10ed4d716fea93297aec89bdff2d002ac
yedam <ydjeong03@gmail.com>
2024-08-22 09:10:14 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import Button from './Button';
 import './App.css';
 
 function App() {
+  const [counter, setCount] = useState(0);
+  const [selectedContent, setSelectedContent] = useState(0); // 새로운 상태 추가
+
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleContentChange = (newValue) => {
+    setSelectedContent(newValue); // Footer에서 전달받은 값으로 상태 업데이트
+  };
+
+  const renderContent = () => {
+    switch (selectedContent) {
+      case 0:
+        return (
+          <>
+            <Content counter={counter} />
+            <Button onClick={onClick} text="Tap" />
+          </>
+        );
+      case 1:
+        return <div>My Record Content</div>;
+      case 2:
+        return <div>Friends Content</div>;
+      default:
+        return <Content counter={counter} />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <div className="content">
+        {renderContent()} {/* 선택된 내용 렌더링 */}
+      </div>
+      <Footer onContentChange={handleContentChange} /> {/* Footer에 핸들러 전달 */}
     </div>
   );
 }
